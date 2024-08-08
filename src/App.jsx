14026fe06a91f25472e342972a09287efeffde64
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-08-08 09:54:33 +0800

    feat(ui): create Home component and change background color
    - Created Home component to serve as the main landing page
    - Updated global CSS to change the background color

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import viteLogo from "/vite.svg";
 import { Button } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button variant="primary">Hello World</Button>
+      <Home></Home>
     </>
   );
 }
