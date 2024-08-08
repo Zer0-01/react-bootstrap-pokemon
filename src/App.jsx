62dbfdc58c24f9454e9109459fa745fe17c7a14b
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-08-08 22:00:52 +0800

    feat: add Details component and integrate React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Home/Home.jsx";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Details from "./details/Details.jsx";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details" element={<Details />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
