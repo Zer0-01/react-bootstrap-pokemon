0090ebb427a1e2367bc2e23a865f1072da52b3d1
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-08-08 21:00:48 +0800

    feat(api): fetch data from Pokémon API and display on cards
    - Implemented API call to fetch Pokémon data from the Pokémon API
    - Created Card component to display Pokémon data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Button } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Home></Home>
+      <Home />
     </>
   );
 }
